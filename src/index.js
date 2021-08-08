64cc4d17a653a79119625fc093d5a1ccabcc2c27
unknown <zhangs001@cmft.com>
2021-08-08 21:00:48 +0800

    test: modefiy test data

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ const data = {
       pack: {
         name: 'region_1',
         children: [
-          {name: 'unit_1', value: 1},
-          {name: 'unit_2', value: 2},
-          {name: 'unit_3', value: 3},
-          {name: 'unit_4', value: 4}
+          {name: 'node_1', value: 1},
+          {name: 'node_2', value: 2},
+          {name: 'node_3', value: 3},
+          {name: 'node_4', value: 4}
         ]
       }
     },
@@ -19,10 +19,10 @@ const data = {
       pack: {
         name: 'region_2',
         children: [
-          {name: 'unit_5', value: 1},
-          {name: 'unit_6', value: 2},
-          {name: 'unit_7', value: 3},
-          {name: 'unit_8', value: 4}
+          {name: 'node_5', value: 1},
+          {name: 'node_6', value: 2},
+          {name: 'node_7', value: 3},
+          {name: 'node_8', value: 4}
         ]
       }
     },
@@ -31,11 +31,11 @@ const data = {
       pack: {
         name: 'region_3',
         children: [
-          {name: 'unit_9', value: 5},
-          {name: 'unit_10', value: 1},
-          {name: 'unit_11', value: 2},
-          {name: 'unit_12', value: 3},
-          {name: 'unit_13', value: 4}
+          {name: 'node_9', value: 5},
+          {name: 'node_10', value: 1},
+          {name: 'node_11', value: 2},
+          {name: 'node_12', value: 3},
+          {name: 'node_13', value: 4}
         ]
       }
     },
@@ -44,9 +44,9 @@ const data = {
       pack: {
         name: 'region_4',
         children: [
-          {name: 'unit_14', value: 1},
-          {name: 'unit_15', value: 2},
-          {name: 'unit_45', value: 3}
+          {name: 'node_14', value: 1},
+          {name: 'node_15', value: 2},
+          {name: 'node_45', value: 3}
         ]
       }
     },
@@ -55,32 +55,31 @@ const data = {
       pack: {
         name: 'region_5',
         children: [
-          {name: 'unit_16',value: 1},
-          {name: 'unit_17', value: 2},
-          {name: 'unit_18', value: 3},
-          {name: 'unit_19', value: 4},
-          {name: 'unit_20', value: 5}
+          {name: 'node_16',value: 1},
+          {name: 'node_17', value: 2},
+          {name: 'node_18', value: 3},
+          {name: 'node_19', value: 4},
+          {name: 'node_20', value: 5}
         ]
       }
     }
   ],
   links: [
-    { id: 'link_1', source: 'unit_1', target: 'unit_20', type: '调用' },
-    { id: 'link_2', source: 'unit_2', target: 'unit_18', type: '调用' },
-    { id: 'link_3', source: 'unit_3', target: 'unit_16', type: '调用' },
-    { id: 'link_4', source: 'unit_5', target: 'unit_15', type: '调用' },
-    { id: 'link_5', source: 'unit_5', target: 'unit_15', type: '调用' },
-    { id: 'link_6', source: 'unit_6', target: 'unit_18', type: '调用' },
-    { id: 'link_7', source: 'unit_7', target: 'unit_17', type: '调用' },
-    { id: 'link_8', source: 'unit_8', target: 'unit_20', type: '调用' },
-    { id: 'link_9', source: 'unit_9', target: 'unit_10', type: '调用' },
-    { id: 'link_10', source: 'unit_10', target: 'unit_1', type: '关联' },
-    { id: 'link_11', source: 'unit_11', target: 'unit_6', type: '关联' },
-    { id: 'link_12', source: 'unit_12', target: 'unit_5', type: '关联' },
-    { id: 'link_13', source: 'unit_14', target: 'unit_8', type: '关联' },
-    { id: 'link_14', source: 'unit_14', target: 'unit_8', type: '关联' },
-    { id: 'link_16', source: 'unit_16', target: 'unit_20', type: '被调用' },
-    { id: 'link_17', source: 'unit_16', target: 'unit_45', type: '被调用' },
+    { id: 'link_1', source: 'node_1', target: 'node_20', type: '调用' },
+    { id: 'link_2', source: 'node_2', target: 'node_18', type: '调用' },
+    { id: 'link_3', source: 'node_3', target: 'node_16', type: '调用' },
+    { id: 'link_4', source: 'node_5', target: 'node_15', type: '调用' },
+    { id: 'link_5', source: 'node_5', target: 'node_15', type: '调用' },
+    { id: 'link_6', source: 'node_6', target: 'node_18', type: '调用' },
+    { id: 'link_7', source: 'node_7', target: 'node_17', type: '调用' },
+    { id: 'link_8', source: 'node_8', target: 'node_20', type: '调用' },
+    { id: 'link_9', source: 'node_9', target: 'node_10', type: '调用' },
+    { id: 'link_10', source: 'node_10', target: 'node_1', type: '关联' },
+    { id: 'link_11', source: 'node_11', target: 'node_6', type: '关联' },
+    { id: 'link_12', source: 'node_12', target: 'node_5', type: '关联' },
+    { id: 'link_13', source: 'node_14', target: 'node_8', type: '关联' },
+    { id: 'link_14', source: 'node_14', target: 'node_8', type: '关联' },
+    { id: 'link_15', source: 'node_16', target: 'node_45', type: '被调用' },
   ]
 }
 
