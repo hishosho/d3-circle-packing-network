7ba55aa206535aa099a13c9c7be83846e94f404a
zhangshuo <zhangs001@cmft.com>
2021-08-21 23:05:30 +0800

    [Chore]: modify output path

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.[contenthash:8].js',
-    path: path.resolve(__dirname, '../dist/js')
+    path: path.resolve(__dirname, '../dist')
   },
   resolve: {
     alias: {
